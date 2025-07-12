77f30b19204d56e5cd81e262fecac56b63575819
TheLC7 <121449383+TheLC7@users.noreply.github.com>
2025-07-12 14:56:32 -0400

    #

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
-);
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL ou ANON KEY não definidos nas variáveis de ambiente.");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
